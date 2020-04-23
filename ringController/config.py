8e3d4759817c5f8c833fad3cdb0146a7c846dcc0
brettweiss2 <33642585+brettweiss2@users.noreply.github.com>
2020-04-23 14:54:19 -0400

    Delete config.py

--- ringController/config.py
+++
@@ -1,8 +0,0 @@
-DIR = 20	# Direction GPIO Pin
-STEP = 21	# Step GPIO Pin
-LIM = 16	# Limit Switch Pin
-SPR = 48	# Steps per Revolution. (360 / degree per step of stepmotor)
-
-defaultRPS = 2
-
-stepsPerRing = 1000
